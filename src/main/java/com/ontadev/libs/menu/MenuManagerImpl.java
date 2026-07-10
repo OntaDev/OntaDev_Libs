eb8362cf84d68ad508972203045fbdab7f3dd88d
kirill <96546910+BotClity@users.noreply.github.com>
2026-07-11 00:41:13 +0300

    Удаление ненужных импортов

--- src/main/java/com/ontadev/libs/menu/MenuManagerImpl.java
+++ src/main/java/com/ontadev/libs/menu/MenuManagerImpl.java
@@ -5,7 +5,6 @@
 package com.ontadev.libs.menu;
 
 import com.ontadev.libs.ioc.annotation.AutoListener;
-import com.ontadev.libs.ioc.annotation.stereotype.Service;
 import com.ontadev.libs.item.ItemModel;
 import com.ontadev.libs.menu.manager.MenuManager;
 import com.ontadev.libs.menu.task.MenuTask;
