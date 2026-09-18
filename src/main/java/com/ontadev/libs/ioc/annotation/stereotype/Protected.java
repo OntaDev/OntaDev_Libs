0206fa3efb23cc591e96f3da33cf5257246dc643
kirill <96546910+BotClity@users.noreply.github.com>
2026-09-18 17:32:04 +0300

    Новая аннотация для будущей логики

---
+++ src/main/java/com/ontadev/libs/ioc/annotation/stereotype/Protected.java
@@ -0,0 +1,16 @@
+// OntaDev_Libs Plugin
+// Авторские права (c) 2026 OntaDev
+// Лицензия: MIT
+
+package com.ontadev.libs.ioc.annotation.stereotype;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@SuppressWarnings("unused")
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface Protected {
+}
